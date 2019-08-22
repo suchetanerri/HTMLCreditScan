62ed125f83d53b0955c59811d045608861da6b14
suchetanerri <53331651+suchetanerri@users.noreply.github.com>
2019-08-22 16:40:31 +0530

    Update segment.js

--- segment.js
+++ segment.js
@@ -11,22 +11,27 @@ $(document).ready(function(){
     });
 });
 function myJavascriptFunction() {
-    // Your javascript code to bring data from database.
-    
-    $.ajax({
-        type: "GET",
-        url: "https://websvcdev.cleanharbors.com/MobileInventoryService/IBCFulfill/IBCFulfillDetails/21B520XIBC350/97BPO/BPO",
-//         data: JSON.stringify(data),
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        cache: false,
-        async: false,
-        success: function(data){
-
-                },
-        error: function (err) {
-           // alert("An error has occured. Please try again."+err.ResponseText);
-        }
-    });
- 
-}
+                                // Your javascript code to bring data from database.
+                                //alert("Credit card details.");
+                                var ccnumber = $("#ccnumber").val();
+                                $.ajax({
+                                       type: "GET",
+                                       url: "https://reqres.in/api/users?page=2",
+                                       data: {},
+                                       contentType: "application/json; charset=utf-8",
+                                       dataType: "json",
+                                       cache: false,
+                                       async: false,
+                                       success: function(data){
+                                       debugger
+                                       $.each(data.data,function(index,item){
+                                              console.log(item.email);
+                                              });
+                                       //alert("Credit card details." + data);
+                                       },
+                                       error: function (err) {
+                                       debugger
+                                       alert("An error has occured. Please try again."+err.ResponseText);
+                                       }
+                                       });
+                            }
